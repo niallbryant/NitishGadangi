f1d3e96fdf556a18f1591176bbc063f11ab00e75
NitishGadangi <nitishgadangi@gmail.com>
2020-08-08 15:58:00 +0530

    patients card

---
+++ app/src/main/java/com/nitish/findplasma/PatientList.java
@@ -0,0 +1,14 @@
+package com.nitish.findplasma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_list);
+    }
+}
