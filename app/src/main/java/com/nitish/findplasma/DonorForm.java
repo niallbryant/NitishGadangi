d6e958124b4706e9a19827e4f55d169bbd8d3c93
NitishGadangi <nitishgadangi@gmail.com>
2020-08-09 00:08:15 +0530

    Donor Form

---
+++ app/src/main/java/com/nitish/findplasma/DonorForm.java
@@ -0,0 +1,14 @@
+package com.nitish.findplasma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonorForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_form);
+    }
+}
