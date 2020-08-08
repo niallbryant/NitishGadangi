6893e807a9ef427d3e7c7912bc93f43213acd292
NitishGadangi <nitishgadangi@gmail.com>
2020-08-09 00:57:52 +0530

    Faqs Page Boiler Plate

---
+++ app/src/main/java/com/nitish/findplasma/Faqs.java
@@ -0,0 +1,17 @@
+package com.nitish.findplasma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Faqs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faqs);
+
+
+
+    }
+}
