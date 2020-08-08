825b8ef204ad17468406e9e130b5c1b86ff15be0
NitishGadangi <nitishgadangi@gmail.com>
2020-08-08 20:31:39 +0530

    show donor patient

---
+++ app/src/main/java/com/nitish/findplasma/ShowDonor.java
@@ -0,0 +1,15 @@
+package com.nitish.findplasma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowDonor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_donor);
+
+    }
+}
---
+++ app/src/main/java/com/nitish/findplasma/ShowPatient.java
@@ -0,0 +1,14 @@
+package com.nitish.findplasma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowPatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_patient);
+    }
+}
