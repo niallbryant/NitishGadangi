71d5b347c370eca32de5bff2db7c48f3a075a756
NitishGadangi <nitishgadangi@gmail.com>
2020-08-09 12:40:51 +0530

    TabLayout in the home (boiler plate)

--- app/src/main/java/com/nitish/findplasma/MainActivity.java
+++ app/src/main/java/com/nitish/findplasma/MainActivity.java
@@ -1,8 +1,16 @@
 package com.nitish.findplasma;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +19,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        FragmentPagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                if (position == 0)
+                    return new FragDonorsList();
+                else
+                    return new FragPatientsList();
+            }
+            @Override
+            public int getCount() {
+                return 2;
+            }
+
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                String title = null;
+                if (position == 0)
+                {
+                    title = "Donors";
+                }
+                else if (position == 1)
+                {
+                    title = "Patients";
+                }
+                return title;
+            }
+        };
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+        viewPagerTab.setViewPager(viewPager);
     }
 }
